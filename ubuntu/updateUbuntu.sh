241159eef46152439b04706651aefd059fdaabec
tyzen9 <steve@tyzen9.com>
2025-03-24 13:04:49 -0400

    added sudo commands

--- ubuntu/updateUbuntu.sh
+++ ubuntu/updateUbuntu.sh
@@ -5,11 +5,11 @@ echo -n "Are you sure you want to continue? (y/N): "
 read answer
 
 if [[ "$answer" =~ ^[Yy]$ ]]; then
-    apt-get update
-    apt-get upgrade -y
-    apt-get full-upgrade -y
-    apt-get autoremove -y
-    apt-get autoclean -y
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    sudo apt-get full-upgrade -y
+    sudo apt-get autoremove -y
+    sudo apt-get autoclean -y
 else
     echo "You chose no (or invalid input). Aborting..."
     exit 1
